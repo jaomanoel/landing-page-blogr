0aada711e80b8086ad9ee99db22167f8369ebd35
jaomanoel <89238095+jaomanoel@users.noreply.github.com>
2023-03-21 20:10:47 -0300

    Created component Hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./ui/components/surface/Header/Header";
+import Hero from "./ui/partials/index/_hero";
 
 function App() {
     return (
         <div className="App bg-slate-500 h-[100vh]">
-            <div className="w-full flex flex-col items-center justify-center rounded-bl-[160px] md:rounded-bl-full bg-light-red">
+            <div className="w-full flex flex-col items-center justify-center rounded-bl-[50px] gap-60 md:gap-120 md:rounded-bl-[50px] bg-light-red">
                 <Header />
+                <Hero />
             </div>
         </div>
     );
---
+++ src/ui/partials/index/_hero.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Button from "../../components/inputs/Button/Button";
+
+const hero: React.FC = () => {
+    return (
+        <div className="w-full flex items-center justify-center px-16 md:px-0 pb-90">
+            <div className="w-full flex flex-col items-center justify-center gap-36">
+                <div className="flex items-center justify-center flex-col">
+                    <h1 className="text-center text-white font-overpass font-semibold w-336 md:w-full text-4xl md:text-6xl">
+                        A modern publishing platform
+                    </h1>
+                    <p className="text-center w-300 md:w-full font-overpass text-lg text-white">
+                        Grow your audience and build your online brand
+                    </p>
+                </div>
+
+                <div className="flex items-center gap-16">
+                    <Button
+                        label="Start for Free"
+                        style="bg-white text-light-red px-24 hover:text-white hover:bg-very-light-red"
+                    />
+                    <Button
+                        label="Learn More"
+                        style="text-white px-24 border border-solid hover:bg-white hover:text-very-light-red"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default hero;
