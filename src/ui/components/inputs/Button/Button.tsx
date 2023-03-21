0bd76b061436838f34a8bb0f0918e5bd75bd4232
jaomanoel <89238095+jaomanoel@users.noreply.github.com>
2023-03-21 19:43:58 -0300

    Update Button

--- src/ui/components/inputs/Button/Button.tsx
+++ src/ui/components/inputs/Button/Button.tsx
@@ -3,15 +3,17 @@ import React from "react";
 export interface ButtonProps {
     label: string;
     style?: string;
+    url?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, style }) => {
+const Button: React.FC<ButtonProps> = ({ label, style, url }) => {
     return (
-        <div
+        <a
+            href={`${url != null ? url : "#"}`}
             className={`max-w-xs rounded-full font-bold py-8 font-ubuntu text-base ${style}`}
         >
             {label}
-        </div>
+        </a>
     );
 };
 
